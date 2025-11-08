be8a473dba0d30d4481a4178283d0b100f90d647
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-08 09:42:45 +0100

    aggiustato scope delle funzioni - dichiarati degli array al di fuori di esse

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,11 +1,11 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const namesInitials = [];
 
 
 // Dichiara la funzione qui.
 function getInitials (namesArray) {
+    let namesInitials = [];
     for (let i = 0; i < namesArray.length; i++) {
         namesInitials.push(namesArray[i][0]);
     }
