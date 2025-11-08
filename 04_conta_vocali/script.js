352b1fa8b08b3081bbbec5c94d83075b8a02011f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-08 09:49:09 +0100

    completato 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,19 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function getNumOfVowels(str) {
+    let vocali = ['a', 'e', 'i', 'o', 'u'];
+    let vowelNum = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vocali.includes(str[i])) {
+            vowelNum++;
+        }
+    }
+    return vowelNum;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getNumOfVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
