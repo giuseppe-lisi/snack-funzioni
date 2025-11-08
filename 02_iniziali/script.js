1d7e89a2c2257e3eb4d00452b1eb7e4bbcca2e6a
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-08 09:32:48 +0100

    completato 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const namesInitials = [];
 
 
 // Dichiara la funzione qui.
-
+function getInitials (namesArray) {
+    for (let i = 0; i < namesArray.length; i++) {
+        namesInitials.push(names[i][0]);
+    }
+    return namesInitials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
